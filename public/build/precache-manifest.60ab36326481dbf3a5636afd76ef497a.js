936705e1850bf99e9a04b2adab580e405fe93df3
Mitchell Rhoads <mrhoads@tunego.com>
2020-09-07 19:37:34 -0700

    minor update

---
+++ public/build/precache-manifest.60ab36326481dbf3a5636afd76ef497a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a63dd818790caffcbea8e3a2fbfcf82e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7e2ca253e0edaca000b2",
+    "url": "/static/css/main.266173d9.chunk.css"
+  },
+  {
+    "revision": "0710684dbcfd3b82bb78",
+    "url": "/static/js/2.37f6cbf8.chunk.js"
+  },
+  {
+    "revision": "a111356e143a232961e42224023a7c84",
+    "url": "/static/js/2.37f6cbf8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e2ca253e0edaca000b2",
+    "url": "/static/js/main.43c037a0.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "21ab7959d38ad88f9780668ce9fd0be5",
+    "url": "/static/media/SpaceX-logo.21ab7959.jpg"
+  }
+]);
